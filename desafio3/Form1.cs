8c4f1b90807c132cbd04d46c5a459424a3a2a865
ariel <santa.cruz.unla@gmail.com>
2023-06-27 19:51:22 -0300

    Agregar archivos de proyecto.

---
+++ desafio3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio3
+{
+    public partial class Form1 : Form
+    {
+        List<Producto> productos;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            productos = new List<Producto>();
+        }
+
+        private void btnAgregarProd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ desafio3/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio3
+{
+    internal class Producto
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Producto(string id="", string descripcion = "", decimal precio=0, int stock=0)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            Precio = precio;
+            Stock = stock;
+        }
+    }
+}
